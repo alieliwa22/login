3078c122968fcf4688c762bd526fe94f317e0dc5
Ali Eliwa <56533995+alieliwa22@users.noreply.github.com>
2021-10-12 03:39:38 +0200

    Add files via upload

---
+++ project-commerce/js/script.js
@@ -0,0 +1,16 @@
+const loginBtn = document.getElementById("login-btn");
+const registerBtn = document.getElementById("register-btn");
+const loginSection = document.getElementById("login-section");
+const registerSection = document.getElementById("register-section");
+loginBtn.addEventListener("click", () => {
+    loginSection.classList.remove("displayn");
+    registerSection.classList.add("displayn");
+    loginBtn.classList.add("active");
+    registerBtn.classList.remove("active")
+});
+registerBtn.addEventListener("click", () => {
+    loginSection.classList.add("displayn");
+    registerSection.classList.remove("displayn");
+    loginBtn.classList.remove("active");
+    registerBtn.classList.add("active")
+});
